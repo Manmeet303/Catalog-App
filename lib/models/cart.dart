7a1b9158c40c70a7560004763cb029cd16ba63d9
Manmeet303 <detrojamanmeet303@gmail.com>
2023-02-16 01:12:07 +0530

    Created singleton for catalog and cartmodel for retriveing same object and now we can also add the cartpage dynamically

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,11 +1,17 @@
 import 'package:myfirstproject/models/catalog.dart';
 
 class CartModel {
+// Creating a singleton of this model so that new objects or the objects refering  the same model can retrive the same current running object instead of always creating a new object
+  static final cartModel = CartModel._internal();
+  CartModel._internal();
+  //The below line means that whenever you call the CartModel than we have to return this cartModel object
+  factory CartModel() => cartModel;
+
   //Catalog Field and created the model
   CatalogModel _catalog;
 
   //Collection of IDs - Store IDs of each Item
-  final List<int> _itemIDs = [];
+  final List<int> _itemIDs = []; 
 
   //Get method to obtain the above private _catalog
   CatalogModel get catalog => _catalog;
--- lib/widgets/Home_widgets/catalogItem.dart
+++ lib/widgets/Home_widgets/catalogItem.dart
@@ -1,9 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:myfirstproject/Screen/HomePage.dart';
-import 'package:myfirstproject/widgets/Home_widgets/catalogImage.dart';
-import 'package:myfirstproject/models/catalog.dart';
-import 'package:myfirstproject/widgets/theme.dart';
 import 'package:velocity_x/velocity_x.dart';
+
+import 'package:myfirstproject/Screen/HomePage.dart';
+import 'package:myfirstproject/models/cart.dart';
+import 'package:myfirstproject/models/catalog.dart';
+import 'package:myfirstproject/widgets/Home_widgets/catalogImage.dart';
+import 'package:myfirstproject/widgets/theme.dart';
 
 class CatalogItem extends StatelessWidget {
   final Item catalog;
@@ -35,18 +38,46 @@ class CatalogItem extends StatelessWidget {
               // buttonPadding: Vx.mH8,
               children: [
                 "\$${catalog.price}".text.bold.lg.make(),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                            context.theme.shadowColor),
-                        shape: MaterialStateProperty.all(StadiumBorder())),
-                    child: "Add to cart".text.make())
+                _AddToCart(catalog: catalog)
               ],
             ).pOnly(right: 8.0)
           ],
         ))
       ],
     )).color(context.theme.cardColor).rounded.square(140).make().py12();
+  }
+}
+
+class _AddToCart extends StatefulWidget {
+  final Item catalog;
+  const _AddToCart({
+    Key key,
+    @required this.catalog,
+  })  : assert(catalog != null),
+        super(key: key);
+
+  @override
+  State<_AddToCart> createState() => _AddToCartState();
+}
+
+class _AddToCartState extends State<_AddToCart> {
+  bool isAdded = false;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          isAdded = isAdded.toggle();
+          final _catalog = CatalogModel();
+          final _cart = CartModel();
+          // Coz we had defined the set and get method so we need to add this
+          _cart.add(widget.catalog);
+          _cart.catalog = _catalog;
+          setState(() {});
+        },
+        style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all(context.theme.shadowColor),
+            shape: MaterialStateProperty.all(StadiumBorder())),
+        child: isAdded ? Icon(Icons.done) : "Add to cart".text.make());
   }
 }
